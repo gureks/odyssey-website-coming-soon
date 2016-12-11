73b963716754afe6ca13a17cde625810f183cb78
Gurek Singh <gurek15033@iiitd.ac.in>
2016-12-11 18:29:15 +0530

    left-col

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('body').countdown('2017/01/01', function(event) {
+	var offset = event.offset;
+	$('#cday').text(offset.totalDays);
+	$('#chours').text(''.concat(offset.hours < 10 ? '0' : '', offset.hours));
+	$('#cminutes').text(''.concat(offset.minutes < 10 ? '0' : '', offset.minutes));
+	$('#cseconds').text(''.concat(offset.seconds < 10 ? '0' : '', offset.seconds));
+});
