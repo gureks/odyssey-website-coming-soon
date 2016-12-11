4f8061b3c8f768fb042414df33ca7139f3c7c1b1
Gurek Singh <gurek15033@iiitd.ac.in>
2016-12-11 18:37:16 +0530

    fixed particles and loader

--- js/main.js
+++ js/main.js
@@ -5,3 +5,7 @@ $('body').countdown('2017/01/01', function(event) {
 	$('#cminutes').text(''.concat(offset.minutes < 10 ? '0' : '', offset.minutes));
 	$('#cseconds').text(''.concat(offset.seconds < 10 ? '0' : '', offset.seconds));
 });
+
+$(document).ready(function(){
+	$(".loader").fadeOut(2000);
+});
