ec7bcb5194e6964f37bc6856ef9fa82fdb153bfa
Gurek Singh <gurek15033@iiitd.ac.in>
2016-12-13 01:36:20 +0530

    Added backend

---
+++ odyssey.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask('__name__')
+app.config['SECRET_KEY'] = os.urandom(20)
+
+@app.route('/')
+def index():
+	if(request.args.get('alert')!=None):
+		return render_template('index.html',alert=request.args.get('alert'))
+	else:
+		return render_template('index.html',alert=None)
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+	file = open("subscribers.txt","a")
+	file.write(request.form['subscribe-email']+"\n")
+	file.close()
+	return redirect(url_for('index',alert="You have been subscribed. We'll keep you notified."))
+
+@app.route('/subscribers')
+def subscribers():
+	file = open('subscribers.txt').readlines()
+	output = ""
+	for line in file:
+		output += line + "<br />"
+	return output
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port=8000,debug=True)
